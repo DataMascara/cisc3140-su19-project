36e6cd4b4bc722705bdc3b983e35ef6173f833fa
Mark <markgoldstein12@gmail.com>
2019-07-23 10:41:10 -0400

    Added Support for post and get

--- app/main.py
+++ app/main.py
@@ -3,38 +3,40 @@ import requests, os, json
 
 app = Flask(__name__)
        
+#Grab the dummy db from JSON file in static folder       
 def data_base():
     filename = os.path.join(app.static_folder, 'db.json')
     with open(filename, 'r') as dbfile:
         data = json.load(dbfile)
     return data
 
+# Grab the users from the "db"
 users_db = data_base()['users']
 
+#Takes in the User DB, username and password to validate
 def validate_user(the_db, username, password):
+    # Go though the users in the db
     for user in the_db:
+        # If the username given and password match
         if( user['username'].lower() == username.lower() and user['password'].lower() ==password.lower()):
-            return True
-    return False
+            return True # Return true
+            #Otherwise, keep going
+    return False # If we reach the end, return false(Either user not found or not valid pw)
 
-print(validate_user(users_db, 'appleMan', "1234"))
 
 @app.route('/', methods=['POST', 'GET'])
 def login():
-    loggedIn = False
-    res = request.form
-    user = res['username']
-    if (res):
-        loggedIn = True
-    password = res['password']
-    if(user in str(users_db)):
-        return render_template('home.html', loggedIn=loggedIn, username=user)
-    users_db.append(user)
-    print(users_db)
+    if(request.method == 'POST'):
+        res = request.form
+        if (res): #Flag for being logged in or no
+            loggedIn = True
+        # Grab the user and pw
+        user = res['username']
+        password = res['password']
+        if(validate_user(users_db,user,password)):
+            return render_template('home.html', loggedIn=loggedIn, username=user)
+        return render_template('home.html')
     return render_template('home.html')
-
-
-
 
 if(__name__ == "__main__"):
         app.run(debug=True)
