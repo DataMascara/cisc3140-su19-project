570ad6a4814fad06d3794b9bb7d6c83c04260ca4
Andrew Campagna <40666746+andrewalexgit@users.noreply.github.com>
2019-07-31 22:53:49 -0400

    API Schema

---
+++ api_schema.py
@@ -0,0 +1,27 @@
+{
+	"<USER_ID>": {
+
+		"posts": [
+
+			"post #1",
+			"post #2",
+			"post #3"
+
+		]
+
+	},
+
+	"<USER_ID>": {
+
+		"posts": [
+
+			"post #1",
+			"post #2",
+			"post #3"
+
+		]
+
+	}
+}
+
+Simple schema for API
