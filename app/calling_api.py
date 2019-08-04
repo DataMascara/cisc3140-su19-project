16b1eb831951ec8ac6ccbd2e06d22cc62d6f5e7e
Mark <markgoldstein12@gmail.com>
2019-08-04 13:44:48 -0400

    changed path for html assets, now render's css ect and calling api works properly

--- app/calling_api.py
+++ app/calling_api.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 import requests
 import json
 import urllib.request
@@ -7,14 +7,14 @@ import webbrowser
 # Example calling the API from another python file
 
 # Relative path to reach the templates folder
-app = Flask(__name__, template_folder=r'./templates/Base Templates/templates')
+app = Flask(__name__)
 
 # Assuming the API is running at the local ip below
 api = "http://127.0.0.1:5000/"
 
 # Now run this file and navigate to the route below
 # Try with chalshaff12 ie /user/
-@app.route('/user/<username>')
+@app.route('/user/<username>') 
 def test(username):
     # Get the response from the API from the /user endpoint
     res = (requests.get(f"{api}user", json={
@@ -28,7 +28,7 @@ def test(username):
 @app.route('/login/')
 def login():
 
-    return render_template('baseLoggedIn.html', title="Logged In :) ")
+    return render_template('baseLoggedOut.html', title="Logged Out :) ")
 
 
 if __name__ == "__main__":
