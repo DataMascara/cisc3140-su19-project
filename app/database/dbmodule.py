ef379bd73486bf7bbac2e735b01279836708f564
Michal Shaffer <michal@dailysteals.com>
2019-08-07 18:42:52 -0400

    added db functions and routes for backend testing

--- app/database/dbmodule.py
+++ app/database/dbmodule.py
@@ -26,148 +26,477 @@ def dbconnection():
 
 	return mydb
 
+class subscriptions_db:
+	#by username, portname, or portid
+	def all_subscriptions_by(column_name, data_value):	
 
-#no input, returns all active users
-#returns fields: userid, username, email, first, last, avatarUrl
-def allUsers():	
+		mydb = dbconnection()
+		#create db cursor
+		cursor = mydb.cursor(buffered=True)
+		#sql statement
+		sql = f"SELECT * FROM subscriptions_vs WHERE {column_name} = '{data_value}'"
 
-	mydb = dbconnection()
-	#create db cursor
-	cursor = mydb.cursor(buffered=True)
-	#sql statement
-	sql = '''SELECT * FROM users where isActive = 1'''
+		try:
+			cursor.execute(sql)
+		except mysql.connector.Error as err:
+			return json.dumps({'error':str(err)})
 
-	try:
-		cursor.execute(sql)
-	except mysql.connector.Error as err:
-		return json.dumps({'error':str(err)})
+		result_set = cursor.fetchall()		#save sql result set
+		#convert columns and rows into json data
+		json_data = [dict(zip([key[0] for key in cursor.description], row)) for row in result_set]
+		#close database connection
 
-	resultSet = cursor.fetchall()		#save sql result set
-	#convert columns and rows into json data
-	jsonData = [dict(zip([key[0] for key in cursor.description], row)) for row in resultSet]
-	#close database connection
+		cursor.close()
+		mydb.close()
 
-	cursor.close()
-	mydb.close()
+		#catch datetime datatype error for json
+		def myconverter(o):
+			if isinstance(o, datetime.datetime):
+				return o.__str__()
+		return json.dumps({'all_subscriptions for {data_value}':json_data}, default = myconverter)
 
-	#catch datetime datatype error for json
-	def myconverter(o):
-		if isinstance(o, datetime.datetime):
-			return o.__str__()
-	return json.dumps({'users':jsonData}, default = myconverter)
+	#input: email (string), password (hashed string), username (string), first (String), last (string), description (string), avatarUrl (string)
+	#email and username must be unique (use find_user)
+	#password should be hashed
+	#all fields are required!!
+	def add_subscription(username, port_id):
 
+		mydb = dbconnection()
+		cursor = mydb.cursor(buffered=True)
 
-#input: columnValue (string), valueValue (string or int) 
-#options and types:
-#columnValue: valueValue
-#id: int
-#username: string
-#email: string
-#output: userid, username, email, first, last, avatarUrl
-def findUsers(columnValue, valueValue):
+		sql = f"INSERT INTO subscriptions (userId, portId) VALUES ((SELECT id FROM users WHERE username = '{username}'), {port_id})"
+		
+		try:
+			cursor.execute(sql)
+			mydb.commit()
+		except mysql.connector.Error as err:
+			return json.dumps({'error':str(err)})
 
-	mydb = dbconnection()
-	cursor = mydb.cursor(buffered=True)
+		#close database connection
+		cursor.close()
+		mydb.close()
+		
+		def myconverter(o):
+			if isinstance(o, datetime.datetime):
+				return o.__str__()
 
-	sql = f"SELECT * FROM users WHERE {columnValue} = '{valueValue}' and isActive = 1"
-	
-	try:
-		cursor.execute(sql)
-	except mysql.connector.Error as err:
-		return json.dumps({'error':str(err)})
-	
-	resultSet = cursor.fetchall()		#save sql result set
-	#convert columns and rows into json data
-	jsonData = [dict(zip([key[0] for key in cursor.description], row)) for row in resultSet]
-
-	#close database connection
-	cursor.close()
-	mydb.close()
-	#catch datetime datatype error for json
-	def myconverter(o):
-		if isinstance(o, datetime.datetime):
-			return o.__str__()
-
-	return json.dumps({'users':jsonData}, default = myconverter)
+		return all_subscriptions('portId', portId)
 
 
-#input: email (string), password (hashed string), username (string), first (String), last (string), avatarUrl (string)
-#email and username must be unique (use findUser)
-#password should be hashed
-#all fields are required!!
-def insertUser(email, password, username, first, last, avatarurl, description):
 
-	mydb = dbconnection()
-	cursor = mydb.cursor(buffered=True)
+	#input: username (string)
+	def update_subscription(username, port_id, value):
 
-	sql = f"INSERT INTO users (email, password, username, first, last, avatarurl, description) VALUES ('{email}','{password}','{username}','{first}','{last}','{avatarurl}',{description})"
-	
-	try:
-		cursor.execute(sql)
-		mydb.commit()
-	except mysql.connector.Error as err:
-		return json.dumps({'error':str(err)})
+		mydb = dbconnection()
+		cursor = mydb.cursor(buffered=True)
 
-	#close database connection
-	cursor.close()
-	mydb.close()
-	
-	def myconverter(o):
-		if isinstance(o, datetime.datetime):
-			return o.__str__()
+		sql = f"UPDATE subscriptions SET isActive = {value} WHERE userId = (SELECT id FROM users WHERE username = '{username}') and portId = {port_id}"
+		
+		try:
+			cursor.execute(sql)
+			mydb.commit()
+		except mysql.connector.Error as err:
+			return json.dumps({'error':str(err)})
+		
+		#close database connection
+		cursor.close()
+		mydb.close()
+			
+		def myconverter(o):
+			if isinstance(o, datetime.datetime):
+				return o.__str__()
 
-	return findUsers('username', username)
+		return f"subscription {username} to port {port_id} is updated"
+		
+
+class ports_db:
+
+	def all_ports():
+
+		mydb = dbconnection()
+		#create db cursor
+		cursor = mydb.cursor(buffered=True)
+		#sql statement
+		sql = '''SELECT id, name, description FROM ports where isActive = 1'''
+
+		try:
+			cursor.execute(sql)
+		except mysql.connector.Error as err:
+			return json.dumps({'error':str(err)})
+
+		result_set = cursor.fetchall()		#save sql result set
+		#convert columns and rows into json data
+		json_data = [dict(zip([key[0] for key in cursor.description], row)) for row in result_set]
+		#close database connection
+
+		cursor.close()
+		mydb.close()
+
+		#catch datetime datatype error for json
+		def myconverter(o):
+			if isinstance(o, datetime.datetime):
+				return o.__str__()
+		return json.dumps({'all_ports':json_data}, default = myconverter)
+
+	def add_port(name, description):
+
+		mydb = dbconnection()
+		cursor = mydb.cursor(buffered=True)
+		sql=  f"INSERT INTO ports (name, description) VALUES ('{name}', '{description}')"
+
+		try:
+			cursor.execute(sql)
+			mydb.commit()
+		except mysql.connector.Error as err:
+			return json.dumps({'error':str(err)})
+
+		#close database connection
+		cursor.close()
+		mydb.close()
+		
+		def myconverter(o):
+			if isinstance(o, datetime.datetime):
+				return o.__str__()
+
+		return posts_db.all_ports()
 
 
+class users_db:
+	#no input, returns all active users
+	#returns fields: userid, username, email, first, last, avatarUrl
+	def all_users():	
+
+		mydb = dbconnection()
+		#create db cursor
+		cursor = mydb.cursor(buffered=True)
+		#sql statement
+		sql = '''SELECT * FROM users_vw'''
+
+		try:
+			cursor.execute(sql)
+		except mysql.connector.Error as err:
+			return json.dumps({'error':str(err)})
+
+		result_set = cursor.fetchall()		#save sql result set
+		#convert columns and rows into json data
+		json_data = [dict(zip([key[0] for key in cursor.description], row)) for row in result_set]
+		#close database connection
+
+		cursor.close()
+		mydb.close()
+
+		#catch datetime datatype error for json
+		def myconverter(o):
+			if isinstance(o, datetime.datetime):
+				return o.__str__()
+		return json.dumps({'all_users':json_data}, default = myconverter)
+
+
+	#input: column_name (string), data_value (string or int) 
+	#options and types:
+	#column_name: data_value
+	#user_id: int
+	#username: string
+	#email: string
+	#output: userid, username, email, first, last, avatarUrl
+	#e.g. http://localhost:5000/find_users?column=username&value=chalshaff12
+	#or http://localhost:5000/find_users?column=email&value=chalshaff12@gmail.com
+	def find_users(column_name, data_value):
+
+		mydb = dbconnection()
+		cursor = mydb.cursor(buffered=True)
+
+		sql = f"SELECT * FROM users_vw WHERE {column_name} = '{data_value}'"
+		
+		try:
+			cursor.execute(sql)
+		except mysql.connector.Error as err:
+			return json.dumps({'error':str(err)})
+		
+		result_set = cursor.fetchall()		#save sql result set
+		#convert columns and rows into json data
+		json_data = [dict(zip([key[0] for key in cursor.description], row)) for row in result_set]
+
+		#close database connection
+		cursor.close()
+		mydb.close()
+		#catch datetime datatype error for json
+		def myconverter(o):
+			if isinstance(o, datetime.datetime):
+				return o.__str__()
+
+		return json.dumps({'user':json_data}, default = myconverter)
+
+
+	#input: email (string), password (hashed string), username (string), first (String), last (string), description (string), avatarUrl (string)
+	#email and username must be unique (use find_user)
+	#password should be hashed
+	#all fields are required!!
+	def add_user(email, password, username, first, last, avatarurl):
+
+		mydb = dbconnection()
+		cursor = mydb.cursor(buffered=True)
+
+		sql = f"INSERT INTO users (email, password, username, first, last, description, avatarurl) VALUES ('{email}','{password}','{username}','{first}','{last}', '{description}', '{avatarurl}')"
+		
+		try:
+			cursor.execute(sql)
+			mydb.commit()
+		except mysql.connector.Error as err:
+			return json.dumps({'error':str(err)})
+
+		#close database connection
+		cursor.close()
+		mydb.close()
+		
+		def myconverter(o):
+			if isinstance(o, datetime.datetime):
+				return o.__str__()
+
+		return find_users('username', username)
 
 #input: username (string)
-def deleteUser(username):
+	def update_user(username, column_name, value_name):
 
-	mydb = dbconnection()
-	cursor = mydb.cursor(buffered=True)
+		mydb = dbconnection()
+		cursor = mydb.cursor(buffered=True)
 
-	sql = f"UPDATE users SET isActive = 0 WHERE username = '{username}'"
-	
-	try:
-		cursor.execute(sql)
-		mydb.commit()
-	except mysql.connector.Error as err:
-		return json.dumps({'error':str(err)})
-	
-	#close database connection
-	cursor.close()
-	mydb.close()
+		sql = f"UPDATE users SET {column_name} = '{value_name}' WHERE username = '{username}'"
 		
-	def myconverter(o):
-		if isinstance(o, datetime.datetime):
-			return o.__str__()
+		try:
+			cursor.execute(sql)
+			mydb.commit()
+		except mysql.connector.Error as err:
+			return json.dumps({'error':str(err)})
+		
+		#close database connection
+		cursor.close()
+		mydb.close()
+			
+		def myconverter(o):
+			if isinstance(o, datetime.datetime):
+				return o.__str__()
 
-	return f"user {username} deleted"
+		return find_users('username', username)
+
+	#input: username (string)
+	def delete_user(username):
+
+		mydb = dbconnection()
+		cursor = mydb.cursor(buffered=True)
+
+		sql = f"UPDATE users SET isActive = 0 WHERE username = '{username}'"
+		
+		try:
+			cursor.execute(sql)
+			mydb.commit()
+		except mysql.connector.Error as err:
+			return json.dumps({'error':str(err)})
+		
+		#close database connection
+		cursor.close()
+		mydb.close()
+			
+		def myconverter(o):
+			if isinstance(o, datetime.datetime):
+				return o.__str__()
+
+		return f"user {username} deleted"
+		
+class posts_db:
+
+	#column_name = port_id or author
+	#data_value depends on the column (always a string)
+	#e.g. http://localhost:5000/all_posts_by?column=author&value=chalshaff12
+	#or http://localhost:5000/all_posts_by?column=port_id&value=1
+	def all_posts_by(column_name, data_value):
+
+		mydb = dbconnection()
+		cursor = mydb.cursor(buffered=True)
+		sql=  f"SELECT * FROM posts_vw where {column_name} = '{data_value}'"
+
+		try:
+			cursor.execute(sql)
+		except mysql.connector.Error as err:
+			return json.dumps({'error':str(err)})
+
+		result_set = cursor.fetchall()		#save sql result set
+		#convert columns and rows into json data
+		json_data = [dict(zip([key[0] for key in cursor.description], row)) for row in result_set]
+		#close database connection
+
+		cursor.close()
+		mydb.close()
+
+		#catch datetime datatype error for json
+		def myconverter(o):
+			if isinstance(o, datetime.datetime):
+				return o.__str__()
+		return json.dumps({'posts':json_data}, default = myconverter)	
+
+
+
+	def add_post(title, text, port_id, author):
+
+		mydb = dbconnection()
+		cursor = mydb.cursor(buffered=True)
+		sql=  f"INSERT INTO posts (title, text, portid, userid) VALUES ('{title}','{text}',{port_id}, (select id from users where username = '{author}'))"
+
+		try:
+			cursor.execute(sql)
+			mydb.commit()
+		except mysql.connector.Error as err:
+			return json.dumps({'error':str(err)})
+
+		#close database connection
+		cursor.close()
+		mydb.close()
+		
+		def myconverter(o):
+			if isinstance(o, datetime.datetime):
+				return o.__str__()
+
+		return posts_db.all_posts_by('author', author)
+
+
+	def delete_post(post_id):
+
+		mydb = dbconnection()
+		cursor = mydb.cursor(buffered=True)
+		sql = f"UPDATE posts SET isDeleted = 1 WHERE id = {post_id}"
+
+		try:
+			cursor.execute(sql)
+			mydb.commit()
+		except mysql.connector.Error as err:
+			return json.dumps({'error':str(err)})
+		
+		#close database connection
+		cursor.close()
+		mydb.close()
+			
+		def myconverter(o):
+			if isinstance(o, datetime.datetime):
+				return o.__str__()
+
+		return f"post {post_id} deleted"
+
+	def update_post(post_id, column_name, data_value):
+
+		mydb = dbconnection()
+		cursor = mydb.cursor(buffered=True)
+		sql=  f"UPDATE posts SET {column_name} = '{data_value}' where id = {post_id}"
+
+		try:
+			cursor.execute(sql)
+			mydb.commit()
+		except mysql.connector.Error as err:
+			return json.dumps({'error':str(err)})
+
+		#close database connection
+		cursor.close()
+		mydb.close()
+		
+		def myconverter(o):
+			if isinstance(o, datetime.datetime):
+				return o.__str__()
+
+		return posts_db.all_posts_by('author', post_id)
+
+		
+class comments_db:
+
 	
+	def all_comments_by(column_name, data_value):
 
-def showPortPosts(portName):
+		mydb = dbconnection()
+		cursor = mydb.cursor(buffered=True)
+		sql=  f"SELECT * FROM comments_vw where {column_name} = '{data_value}'"
 
-	mydb = dbconnection()
-	cursor = mydb.cursor(buffered=True)
+		try:
+			cursor.execute(sql)
+		except mysql.connector.Error as err:
+			return json.dumps({'error':str(err)})
 
-	sql=  f"select p.text, u.username as author, v.vote as votes from posts p left join users u on p.userid = u.id left join votes v on v.postid = p.id where p.isDeleted = 0 and p.portid = (select id from ports where name = '{portName}')"
+		result_set = cursor.fetchall()		#save sql result set
+		#convert columns and rows into json data
+		json_data = [dict(zip([key[0] for key in cursor.description], row)) for row in result_set]
+		#close database connection
 
-	try:
-		cursor.execute(sql)
-	except mysql.connector.Error as err:
-		return json.dumps({'error':str(err)})
+		cursor.close()
+		mydb.close()
 
-	resultSet = cursor.fetchall()		#save sql result set
-	#convert columns and rows into json data
-	jsonData = [dict(zip([key[0] for key in cursor.description], row)) for row in resultSet]
-	#close database connection
+		#catch datetime datatype error for json
+		def myconverter(o):
+			if isinstance(o, datetime.datetime):
+				return o.__str__()
+		return json.dumps({'comments':json_data}, default = myconverter)	
 
-	cursor.close()
-	mydb.close()
 
-	#catch datetime datatype error for json
-	def myconverter(o):
-		if isinstance(o, datetime.datetime):
-			return o.__str__()
-	return json.dumps({'posts':jsonData}, default = myconverter)	
-	
+
+	def add_comment(text, post_id, parent_id, author):
+
+		mydb = dbconnection()
+		cursor = mydb.cursor(buffered=True)
+		sql=  f"INSERT INTO comments ('{text}', {post_id}, {parent_id}, (select id from users where username = '{author}'))"
+
+		try:
+			cursor.execute(sql)
+			mydb.commit()
+		except mysql.connector.Error as err:
+			return json.dumps({'error':str(err)})
+
+		#close database connection
+		cursor.close()
+		mydb.close()
+		
+		def myconverter(o):
+			if isinstance(o, datetime.datetime):
+				return o.__str__()
+
+		return posts_db.all_comments('author', author)
+
+
+	def delete_comment(comment_id):
+
+		mydb = dbconnection()
+		cursor = mydb.cursor(buffered=True)
+		sql = f"UPDATE comments SET isDeleted = 1 WHERE id = {comment_id}"
+
+		try:
+			cursor.execute(sql)
+			mydb.commit()
+		except mysql.connector.Error as err:
+			return json.dumps({'error':str(err)})
+		
+		#close database connection
+		cursor.close()
+		mydb.close()
+			
+		def myconverter(o):
+			if isinstance(o, datetime.datetime):
+				return o.__str__()
+
+		return f"comment {comment_id} deleted"
+
+	def update_comment(comment_id, text):
+
+		mydb = dbconnection()
+		cursor = mydb.cursor(buffered=True)
+		sql=  f"UPDATE comments SET text = '{text}' where id = {comment_id}"
+
+		try:
+			cursor.execute(sql)
+			mydb.commit()
+		except mysql.connector.Error as err:
+			return json.dumps({'error':str(err)})
+
+		#close database connection
+		cursor.close()
+		mydb.close()
+		
+		def myconverter(o):
+			if isinstance(o, datetime.datetime):
+				return o.__str__()
+
+		return posts_db.all_comments_by('comment_id', comment_id)
