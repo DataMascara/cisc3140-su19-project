8c04c7b61b742c7352e760acafcfec7383dd991f
StevenGinsberg <53059396+StevenGinsberg@users.noreply.github.com>
2019-08-09 15:31:38 -0400

    Add files via upload

---
+++ Quality Control/dbtest.py
@@ -0,0 +1,23 @@
+from app import app, dbmodule
+import requests
+from flask import request
+from dbmodule import add_port
+
+def testport():
+	assert(add_port(StevenGinsberg,student)== str(resultset))
+
+	def test all_users():
+	assert(all_users(StevenGinsberg,student)== str(resultset))
+def test find_users():
+	assert(find_users(StevenGinsberg,student)== str(resultset))
+def test add_user():
+	assert(add_user(StevenGinsberg,student)== str(resultset))
+def test update_user():
+	assert(update_user(StevenGinsberg,student)== str(resultset))
+def test delete_user():
+	assert(delete_user(StevenGinsberg,student)== str(resultset))
+def  test add_post():
+	assert(add_post('Chocolate chip Cookies','Yum Yum Yum','StevenGinsberg','portid')== str(resultset))
+
+def  test delete_post():
+	assert(delete_post(post_id)== str(resultset))
