43d51ef4f4e4f3f12d31c125d52712d8037002ef
StevenGinsberg <53059396+StevenGinsberg@users.noreply.github.com>
2019-08-09 15:32:49 -0400

    Update dbtest.py

--- Quality Control/dbtest.py
+++ Quality Control/dbtest.py
@@ -1,7 +1,8 @@
 from app import app, dbmodule
+import unittest
 import requests
 from flask import request
-from dbmodule import add_port
+from dbmodule import add_port,find_users,add_user,update_user,delete_user,add_post,delete_post
 
 def testport():
 	assert(add_port(StevenGinsberg,student)== str(resultset))
@@ -21,3 +22,14 @@ def  test add_post():
 
 def  test delete_post():
 	assert(delete_post(post_id)== str(resultset))
+
+def  test add_comment():
+	assert(add_comment('Mike and Ike Cookies','Wow candy and cookies!','StevenGinsberg','portid')== str(resultset))
+
+def  test update_comment():
+	assert(update_comment('Mike and Ike cookies','My teeth hurt!','StevenGinsberg','portid')== str(resultset))
+
+def  test delete_comment():
+	assert(update_post('Mike and Ike cookies','My teeth hurt!','portid')== str(resultset))
+
+  
