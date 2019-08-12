6b35eb00cfcf07f07bf2f416942b47780ced3106
Michal Shaffer <michal@dailysteals.com>
2019-08-12 14:30:11 -0400

    added comments to db create file

--- app/database/CreateDBTables.sql
+++ app/database/CreateDBTables.sql
@@ -1,127 +1,119 @@
 
-#create all tables in the database by executing this file
-
-#users table saves user information
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,   #auto generated
-  `username` varchar(30) NOT NULL,            
-  `first` varchar(30) DEFAULT NULL,           
-  `last` varchar(30) DEFAULT NULL,            
-  `email` varchar(128) NOT NULL,              
-  `password` varchar(128) NOT NULL,
-  `description` varchar(1000) NULL,
-  `isActive` boolean NOT NULL DEFAULT '1',    #check for deleted account
-  `avatarUrl` text DEFAULT NULL,      #hold image url for avatar or profile pic
-  `dateModified` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP, #auto updated
-  `dateCreated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,        #auto generated on row insertion
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`), #username must be unique
-  UNIQUE KEY `email` (`email`)	#email must be unique
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(30) NOT NULL,
+    `first` VARCHAR(30) DEFAULT NULL,
+    `last` VARCHAR(30) DEFAULT NULL,
+    `email` VARCHAR(128) NOT NULL,
+    `password` VARCHAR(128) NOT NULL,
+    `description` VARCHAR(1000) NULL,
+    `isActive` BOOLEAN NOT NULL DEFAULT '1',
+    `avatarUrl` TEXT DEFAULT NULL,
+    `dateModified` DATETIME DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+    `dateCreated` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `username` (`username`),
+    UNIQUE KEY `email` (`email`)
 );
 
-#ports table saves port/forum information
-CREATE TABLE `ports`(
- `id`       integer NOT NULL auto_increment,#auto generated
- `isActive`     boolean NOT NULL default 1,     #is port currently active
- `name`     varchar(30) NOT NULL ,          #unique name for port
- `description` varchar(1000) NULL,
- `dateModified` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP, #auto updated
-`dateCreated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,        #auto generated on row insertion
-  
-PRIMARY KEY (`id`),
-UNIQUE KEY `name` (`name`)	#port name must be unique
+CREATE TABLE `ports` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `isActive` BOOLEAN NOT NULL DEFAULT 1,
+    `name` VARCHAR(30) NOT NULL,
+    `description` VARCHAR(1000) NULL,
+    `dateModified` DATETIME DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+    `dateCreated` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `name` (`name`)
 );
 
-#posts table saves post information
-CREATE TABLE `posts`(
- `id`       integer NOT NULL auto_increment,      #auto generated
- `title`	varchar(100) NOT NULL,				
- `text`     text NOT NULL ,
- `imageUrl` text NULL ,
- `isDeleted`    boolean NOT NULL DEFAULT 0,           #is post deleted or not
- `portId`       integer NOT NULL ,                    #FK referencing the post
- `userId`       integer NOT NULL ,                    #FK referencing the author from users table
- `dateModified` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP, #auto updated
- `dateCreated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,        #auto generated on row insertion
-  
-PRIMARY KEY (`id`),
-KEY `fkIdx_28` (`id`),
-CONSTRAINT `FK_28` FOREIGN KEY `fkIdx_28` (`userId`) REFERENCES `users` (`id`),
-KEY `fkIdx_34` (`portId`),
-CONSTRAINT `FK_34` FOREIGN KEY `fkIdx_34` (`portId`) REFERENCES `ports` (`id`),
-UNIQUE KEY `title` (`title`)
+CREATE TABLE `posts` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(100) NOT NULL,
+    `text` TEXT NOT NULL,
+    `imageUrl` TEXT NULL,
+    `isDeleted` BOOLEAN NOT NULL DEFAULT 0,
+    `portId` INTEGER NOT NULL,
+    `userId` INTEGER NOT NULL,
+    `dateModified` DATETIME DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+    `dateCreated` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    KEY `fkIdx_28` (`id`),
+    CONSTRAINT `FK_28` FOREIGN KEY (`userId`)
+        REFERENCES `users` (`id`),
+    KEY `fkIdx_34` (`portId`),
+    CONSTRAINT `FK_34` FOREIGN KEY (`portId`)
+        REFERENCES `ports` (`id`),
+    UNIQUE KEY `title` (`title`)
 );
 
-#comments table saves comment information
-CREATE TABLE `comments`(
- `id`    integer NOT NULL auto_increment,        #auto generated
- `postId`       integer NOT NULL ,                      #FK referencing the post
- `text`  text NOT NULL ,
- `parentId`     integer NULL ,                          #FK referencing the parent comment
- `isDeleted`    boolean NOT NULL default 0,             #is comment deleted or not
- `userId`       integer NOT NULL ,                      #FK referencing the author from users table
- `dateModified` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP, #auto updated
- `dateCreated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,        #auto generated on row insertion
-  
-
-PRIMARY KEY (`id`),
-KEY `fkIdx_51` (`userId`),
-CONSTRAINT `FK_51` FOREIGN KEY `fkIdx_51` (`userId`) REFERENCES `users` (`id`),
-KEY `fkIdx_54` (`postId`),
-CONSTRAINT `FK_54` FOREIGN KEY `fkIdx_54` (`postId`) REFERENCES `posts` (`id`)
+CREATE TABLE `comments` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `postId` INTEGER NOT NULL,
+    `text` TEXT NOT NULL,
+    `parentId` INTEGER NULL,
+    `isDeleted` BOOLEAN NOT NULL DEFAULT 0,
+    `userId` INTEGER NOT NULL,
+    `dateModified` DATETIME DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+    `dateCreated` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    KEY `fkIdx_51` (`userId`),
+    CONSTRAINT `FK_51` FOREIGN KEY (`userId`)
+        REFERENCES `users` (`id`),
+    KEY `fkIdx_54` (`postId`),
+    CONSTRAINT `FK_54` FOREIGN KEY (`postId`)
+        REFERENCES `posts` (`id`)
 );
 
-#subscriptions table saves subscription information
-CREATE TABLE `subscriptions`(
- `id`       integer NOT NULL auto_increment,     #auto generated
- `isActive`     boolean NOT NULL default 1,          #current subscription or deleted
- `portId`       integer NOT NULL ,                   #FK referencing the port 
- `userId`       integer NOT NULL ,                   #FK referencing the user
- `dateModified` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP, #auto updated
- `dateCreated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,        #auto generated on row insertion
-  
-PRIMARY KEY (`id`),
-KEY `fkIdx_14` (`userId`),
-CONSTRAINT `FK_14` FOREIGN KEY `fkIdx_14` (`userId`) REFERENCES `users` (`id`),
-KEY `fkIdx_17` (`portId`),
-CONSTRAINT `FK_17` FOREIGN KEY `fkIdx_17` (`portId`) REFERENCES `ports` (`id`),
-UNIQUE KEY `uniquePortandUser` (`portId`, `userId`) #doesn't allow duplicate subscriptions
+CREATE TABLE `subscriptions` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `isActive` BOOLEAN NOT NULL DEFAULT 1,
+    `portId` INTEGER NOT NULL,
+    `userId` INTEGER NOT NULL,
+    `dateModified` DATETIME DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+    `dateCreated` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    KEY `fkIdx_14` (`userId`),
+    CONSTRAINT `FK_14` FOREIGN KEY (`userId`)
+        REFERENCES `users` (`id`),
+    KEY `fkIdx_17` (`portId`),
+    CONSTRAINT `FK_17` FOREIGN KEY (`portId`)
+        REFERENCES `ports` (`id`),
+    UNIQUE KEY `uniquePortandUser` (`portId` , `userId`)
 );
 
-#ads table saves ad information
-CREATE TABLE `ads`(
- `id`     integer NOT NULL auto_increment,    #auto generated
- `adUrl`    text NOT NULL ,
- `image` text NOT NULL ,
- `text`   varchar(1000) NOT NULL ,
- `isActive` boolean not null default 0,          #is ad currently active on site or not
- `dateModified` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP, #auto updated
- `dateCreated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,        #auto generated on row insertion
-  
-PRIMARY KEY (`id`)
+CREATE TABLE `ads` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `adUrl` TEXT NOT NULL,
+    `image` TEXT NOT NULL,
+    `text` VARCHAR(1000) NOT NULL,
+    `isActive` BOOLEAN NOT NULL DEFAULT 0,
+    `dateModified` DATETIME DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+    `dateCreated` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
 );
 
-#table to record users' saved posts and upvoted or downvoted posts and comments
-CREATE TABLE `votes`
-(
- `id`      integer NOT NULL AUTO_INCREMENT,    #auto generated
- `userId`      integer NOT NULL , 					#FK referencing the user
- `postId`      integer NULL ,						#FK referencing the post (optional)
- `commentId`   integer NULL ,						#FK referencing the comment (optional) -->either the postid or commentid must be filled
- `isSaved`     boolean NOT NULL DEFAULT 0,			#is comment or post saved
- `vote`  	   integer NOT NULL DEFAULT 0,			#1 for upvote, -1 for downvote, 0 for no vote
- `dateModified` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP, #auto updated
- `dateCreated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,        #auto generated on row insertion
- 
-PRIMARY KEY (`id`),
-KEY `fkIdx_101` (`postId`),
-CONSTRAINT `FK_101` FOREIGN KEY `fkIdx_101` (`postId`) REFERENCES `posts` (`id`),
-KEY `fkIdx_107` (`commentId`),
-CONSTRAINT `FK_107` FOREIGN KEY `fkIdx_107` (`commentId`) REFERENCES `comments` (`id`),
-KEY `fkIdx_98` (`userId`),
-CONSTRAINT `FK_98` FOREIGN KEY `fkIdx_98` (`userId`) REFERENCES `users` (`id`),
-UNIQUE KEY `uniqueUserperPost` (`userId`, `postId`), #only one vote per post per user
-UNIQUE KEY `uniqueUserperComment` (`userId`, `commentId`) #only one vote per comment per user
+CREATE TABLE `votes` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `userId` INTEGER NOT NULL,
+    `postId` INTEGER NULL,
+    `commentId` INTEGER NULL,
+    `isSaved` BOOLEAN NOT NULL DEFAULT 0,
+    `vote` INTEGER NOT NULL DEFAULT 0,
+    `dateModified` DATETIME DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+    `dateCreated` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    KEY `fkIdx_101` (`postId`),
+    CONSTRAINT `FK_101` FOREIGN KEY (`postId`)
+        REFERENCES `posts` (`id`),
+    KEY `fkIdx_107` (`commentId`),
+    CONSTRAINT `FK_107` FOREIGN KEY (`commentId`)
+        REFERENCES `comments` (`id`),
+    KEY `fkIdx_98` (`userId`),
+    CONSTRAINT `FK_98` FOREIGN KEY (`userId`)
+        REFERENCES `users` (`id`),
+    UNIQUE KEY `uniqueUserperPost` (`userId` , `postId`),
+    UNIQUE KEY `uniqueUserperComment` (`userId` , `commentId`)
 );
 
 
@@ -133,7 +125,7 @@ AFTER INSERT
    ON users FOR EACH ROW
 
 BEGIN
-
+	#adds an automatic subscription to 'main' port for every new user
    INSERT INTO subscriptions
    ( userId,
      portId)
@@ -153,7 +145,7 @@ AFTER INSERT
    ON posts FOR EACH ROW
 
 BEGIN
-
+	#adds an automatic upvote for every user's own added post
    INSERT INTO votes
    ( userId,
      postId,
@@ -176,7 +168,7 @@ AFTER INSERT
    ON comments FOR EACH ROW
 
 BEGIN
-
+#adds an automatic upvote for every user's own added comment
    INSERT INTO votes
    ( userId,
      commentId,
@@ -191,73 +183,140 @@ END; //
 DELIMITER ;
 
 
-create view posts_vw as
-    #votes are being cast as char(10) because the integer sums were not showing up through the python functions for some reason
-select pr.name as portName, p.id as postId, p.title as postTitle, p.text as postText, p.imageUrl as image, u.username as author, 
-cast(sum(vote) as char(10)) as votes, (select cast(count(id) as char(10)) from comments c where c.postId = p.id) as numComments, p.dateCreated
-from posts p
-left join users u on p.userid = u.id
-left join votes v on v.postid = p.id
-left join ports pr on pr.id = p.portid
-where p.isDeleted = 0
-group by p.id
-Order by p.id;
+CREATE VIEW posts_vw AS
+    SELECT 
+        pr.name AS portName,
+        p.id AS postId,
+        p.title AS postTitle,
+        p.text AS postText,
+        p.imageUrl AS image,
+        u.username AS author,
+        CAST(SUM(vote) AS CHAR (10)) AS votes,
+        (SELECT 
+                CAST(COUNT(id) AS CHAR (10))
+            FROM
+                comments c
+            WHERE
+                c.postId = p.id) AS numComments,
+        p.dateCreated
+    FROM
+        posts p
+            LEFT JOIN
+        users u ON p.userid = u.id
+            LEFT JOIN
+        votes v ON v.postid = p.id
+            LEFT JOIN
+        ports pr ON pr.id = p.portid
+    WHERE
+        p.isDeleted = 0
+    GROUP BY p.id
+    ORDER BY p.id;
 
-create view comments_vw as
-    #votes are being cast as char(10) because the integer sums were not showing up through the python functions for some reason
-select p.postId, c.id as commentId, c.text as commentText, u.username as author, cast(sum(vote) as char(10)) as votes, parentId
-from comments c 
-left join posts_vw p on c.postid = p.postId
-left join votes v on v.commentid = c.id
-left join users u on u.id = c.userid
-where c.isDeleted = 0
-group by c.id;
+CREATE VIEW comments_vw AS
+    SELECT 
+        p.postId,
+        c.id AS commentId,
+        c.text AS commentText,
+        u.username AS author,
+        CAST(SUM(vote) AS CHAR (10)) AS votes,
+        parentId
+    FROM
+        comments c
+            LEFT JOIN
+        posts_vw p ON c.postid = p.postId
+            LEFT JOIN
+        votes v ON v.commentid = c.id
+            LEFT JOIN
+        users u ON u.id = c.userid
+    WHERE
+        c.isDeleted = 0
+    GROUP BY c.id;CREATE VIEW subscriptions_vw AS
+    SELECT 
+        username, p.id AS portId, p.name AS portName
+    FROM
+        subscriptions s
+            JOIN
+        users u ON u.id = s.userid
+            JOIN
+        ports p ON p.id = s.portid
+    WHERE
+        s.isActive = 1;
 
+CREATE VIEW votes_vw AS
+    SELECT DISTINCT
+        p.id AS postId,
+        p.title AS Title,
+        p.imageUrl AS Image,
+        p.text AS Text,
+        u.username AS author,
+        uv.username AS voteUsername,
+        vote,
+        isSaved,
+        'Post' AS type
+    FROM
+        posts p
+            LEFT JOIN
+        users u ON p.userid = u.id
+            LEFT JOIN
+        votes v ON v.postid = p.id
+            JOIN
+        users uv ON uv.id = v.userid
+    WHERE
+        isDeleted = 0 
+    UNION SELECT DISTINCT
+        c.id AS commentId,
+        '' AS Title,
+        '' AS Image,
+        c.text AS Text,
+        u.username AS author,
+        uv.username AS voteUsername,
+        vote,
+        isSaved,
+        'Comment' AS type
+    FROM
+        comments c
+            LEFT JOIN
+        users u ON u.id = c.userid
+            LEFT JOIN
+        votes v ON v.commentid = c.id
+            JOIN
+        users uv ON uv.id = v.userid
+    WHERE
+        c.isDeleted = 0;
 
-create view subscriptions_vw as
-select username, p.id as portId, p.name as portName
-from subscriptions s 
-join users u on u.id = s.userid
-join ports p on p.id = s.portid
-where s.isActive = 1;
+CREATE VIEW users_vw AS
+    SELECT 
+        u.id AS userId,
+        password,
+        username,
+        first,
+        last,
+        email,
+        description,
+        avatarUrl,
+        CAST(SUM(vote) AS CHAR (10)) AS votes
+    FROM
+        users u
+            LEFT JOIN
+        votes_vw v ON u.username = v.author
+    WHERE
+        u.isActive = 1
+    GROUP BY u.id; #group by userID to get sum of votes per user's posts and comments
 
-create view votes_vw as
-  #votes are being cast as char(10) because the integer sums were not showing up through the python functions for some reason
-select distinct p.id as postId, p.title as Title, p.imageUrl as Image, p.text as Text, u.username as author, uv.username as voteUsername, vote, isSaved, 'Post' as type
-from posts p
-left join users u on p.userid = u.id
-left join votes v on v.postid = p.id
-join users uv on uv.id = v.userid
-where isDeleted = 0
-union
-select distinct c.id as commentId, '' as Title, '' as Image, c.text as Text, u.username as author, uv.username as voteUsername, vote, isSaved, 'Comment' as type
-from comments c
-left join users u on u.id = c.userid
-left join votes v on v.commentid = c.id
-join users uv on uv.id = v.userid
-where c.isDeleted = 0;
-
-
-create view users_vw as
-select u.id as userId, password, username, first, last, email, description, avatarUrl, cast(sum(vote) as char(10)) as votes
-from users u
-left join votes_vw v on u.username = v.author
-where u.isActive = 1
-group by u.id;
 
 
 DELIMITER $$
- 
+#this function adds a post and returns the new post's ID. 
 CREATE FUNCTION add_post(title varchar(360), text varchar(1000), port_name varchar(30), author varchar(30), image text) RETURNS INTEGER
     DETERMINISTIC
 BEGIN
     DECLARE newId INTEGER;
-
+	#insert the post
     INSERT INTO posts (title, text, portId, userid, imageUrl) VALUES 
-    (title, text, (select id from ports where name = port_name), (select id from users where username = author), image);
-
+    (title, text, (SELECT id FROM ports WHERE name = port_name), (SELECT id FROM users WHERE username = author), image);
+	#set the variable to be the new post's ID
  	SET newId =  LAST_INSERT_ID();
-
+	#return the new ID
  RETURN (newId);
 END
 
