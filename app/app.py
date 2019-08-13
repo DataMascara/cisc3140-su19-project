4cfd3cf0f617712ffa6fdd051aad14079e75bd72
Mark Goldstein <markgoldstein12@gmail.com>
2019-08-13 18:42:10 -0400

    api

--- app/app.py
+++ app/app.py
@@ -11,13 +11,13 @@ app.secret_key = "test"
 
 # Assuming the API is running at the local ip below
 api = "https://bc-api-class.herokuapp.com"
+# api = "http://127.0.0.1:5000"
 
 @app.route("/", methods=["GET"])
 def redirect_home():
     if "loggedin" in session:
         return redirect("/home/")
     return redirect("/login/")
-
 
 """
 -------------LOGIN-------------
@@ -92,10 +92,10 @@ def logout():
 @app.route("/home/", methods=["GET"])
 def home():
     # Use the helper method to grab "tredning ports"
-    trending = trending_ports()["all_ports"]
+    trending = trending_ports()['all_ports']
     port = requests.get(f"{api}/posts-by-portname/", json={"portname": "Main"}).json()
+    print(port)
     if "loggedin" in session:
-
         update_vote_for_post(port)
         return render_template(
             "posts.html",
@@ -123,7 +123,7 @@ def home():
 def portpost(portname):
     port = requests.get(f"{api}/posts-by-portname/", json={"portname": portname}).json()
     print(type(port))
-    trending = trending_ports()["all_ports"]
+    trending = trending_ports()['all_ports']
     if "loggedin" in session:
         update_vote_for_post(port)
         return render_template(
@@ -693,15 +693,18 @@ def pending():
 # WIll do this by just getting three three random ports
 def trending_ports():
     ports = requests.get(f"{api}/allports/").json()
-    if "loggedin" in session:
-        for port in ports['all_ports']:
-            for subscribe_ports in session['subscriptions']:
-                if port['id'] == subscribe_ports['portId']:
-                    port.update({"isSubscribed": True})
-                    break
-    # Add way of deciding what ports are "trending"
-    # Return a dictonary of the port representation
+    # if "loggedin" in session:
+        # for port in ports['all_ports']:
+    #         for subscribe_ports in session['subscriptions']:
+    #             if port['id'] == subscribe_ports['portId']:
+    #                 port.update({"isSubscribed": True})
+    #                 break
+    # # Add way of deciding what ports are "trending"
+    # # Return a dictonary of the port representation
+    # print(list(port))
+    # print(len(list(port)))
     return ports
+
 
 
 def update_vote_for_post(port):
