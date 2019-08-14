00370919b5fbb2bb0d19c3c78b76a8fcbd724747
Miriam Briskman <52957236+mary060196@users.noreply.github.com>
2019-08-14 16:31:36 -0400

    Setting URL Length

--- app/static/js/scriptsWritePost.js
+++ app/static/js/scriptsWritePost.js
@@ -13,3 +13,18 @@ function changeVal ()
        else
                  document.getElementsByClassName("imageLabel")[0].innerHTML = "Add Image";
 }
+
+// Make Received URLs no longer than 300 chars:
+function URLLength ()
+{
+     if (document.getElementsByClassName("addimage")[0].value.length > 0)
+     {
+          document.getElementsByClassName("addimage")[0].required = true;
+          if (document.getElementsByClassName("addimage")[0].value.length > 300)
+               document.getElementsByClassName("addimage")[0].setCustomValidity("URL must be no longer than 300 chars!");
+          else
+               document.getElementsByClassName("addimage")[0].setCustomValidity("");
+     }
+     else
+          document.getElementsByClassName("addimage")[0].required = false;
+}
