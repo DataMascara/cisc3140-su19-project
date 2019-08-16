43ff1608153fe58079baee22209c2d620362cc3d
Miriam Briskman <52957236+mary060196@users.noreply.github.com>
2019-08-16 17:54:28 -0400

    Update scriptsWritePost.js

--- app/static/js/scriptsWritePost.js
+++ app/static/js/scriptsWritePost.js
@@ -32,5 +32,48 @@ function URLLength ()
 // Escape the necessary characters:
 function escapeChars (x)
 {
-    x.value = x.value.replace(/\\/g,'\\\\').replace(/\n/g,'\\\n').replace(/\f/g,'\\\f').replace(/\r/g,'\\\r').replace(/\t/g,'\\\t').replace(/\v/g,'\\\v').replace(/\0/g,'\\\0').replace(/\'/g,'\\\'').replace(/\"/g,'\"');
+    for (i = 0; i < x.value.length; i++)
+    {
+        if (x.value[i] == "\\")
+        {
+            x.value[i] = "\\\\";
+            i++;
+        }
+        else if (x.value[i] == "\n")
+        {
+            x.value[i] = "\\\n";
+            i++;
+        }
+        else if (x.value[i] == "\f")
+        {
+            x.value[i] = "\\\f";
+            i++;
+        }
+        else if (x.value[i] == "\r")
+        {
+            x.value[i] = "\\\r";
+            i++;
+        }
+        else if (x.value[i] == "\v")
+        {
+            x.value[i] = "\\\v";
+            i++;
+        }
+        else if (x.value[i] == "\t")
+        {
+            x.value[i] = "\\\t";
+            i++;
+        }
+        else if (x.value[i] == "\0")
+        {
+            x.value[i] = "\\\0";
+            i++;
+        }
+        else if (x.value[i] == "\'")
+        {
+            x.value[i] = "\\\'";
+            i++;
+        }
+    //x.value = x.value.replace(/\\/g,'\\\\').replace(/\n/g,'\\\n').replace(/\f/g,'\\\f').replace(/\r/g,'\\\r').replace(/\t/g,'\\\t').replace(/\v/g,'\\\v').replace(/\0/g,'\\\0').replace(/\'/g,'\\\'').replace(/\"/g,'\"');
+    }    
 }
