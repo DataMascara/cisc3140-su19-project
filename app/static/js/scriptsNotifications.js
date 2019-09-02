8c2a04e3e6a91b1593e8781ee6b4d00fdfeea608
Miriam Briskman <52957236+mary060196@users.noreply.github.com>
2019-09-01 22:39:56 -0400

    JS File for Notifications Template Added

---
+++ app/static/js/scriptsNotifications.js
@@ -0,0 +1,13 @@
+﻿// A function that shortens the length of the post title and comment content if they are longer than 50 chars:
+function cutIt ()
+{
+    for (i = 0; i < document.getElementsByClassName("nameOfPost").length; i++)
+    {
+         if (document.getElementsByClassName("nameOfPostOrComment")[i].innerHTML.length > 50)
+                document.getElementsByClassName("nameOfPostOrComment")[i].innerHTML 
+                             = document.getElementsByClassName("nameOfPostOrComment")[i].innerHTML.substr(0, 49) + "...";
+         if (document.getElementsByClassName("theNoteContent")[i].innerHTML.length > 50)
+                document.getElementsByClassName("theNoteContent")[i].innerHTML 
+                             = document.getElementsByClassName("theNoteContent")[i].innerHTML.substr(0, 49) + "...";
+    }
+}
