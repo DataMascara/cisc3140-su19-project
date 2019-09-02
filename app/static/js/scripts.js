8f688efb8e766b8f6e338844a21dcda4363b4da6
Miriam Briskman <52957236+mary060196@users.noreply.github.com>
2019-09-02 02:10:52 -0400

    "Go to Top" Button Functionality Added

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -90,3 +90,21 @@ function displayYear()
 {
      document.getElementsByClassName("year")[0].innerHTML = (new Date()).getFullYear();
 }
+
+// When the user scrolls down 100px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
